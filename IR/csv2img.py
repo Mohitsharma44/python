9724c425d43ed9ee97ff19089f555a5187f916e8
Mohit Sharma <mohitsharma44@gmail.com>
2014-11-21 01:03:12 -0500

    Realtime Video from numpy arrays

--- IR/csv2img.py
+++ IR/csv2img.py
@@ -3,44 +3,63 @@ from matplotlib import pyplot as plt
 import numpy as np
 import PIL
 import matplotlib.animation as animation
-from pylab import *
+import pylab as pl
+import time
 
-def csv2np(path):
-    csvfile = genfromtxt(path,delimiter=',')
-    cols = csvfile.shape[0]
-    frames = cols/240
+def csv2np():
+    pl.ion()
+    pl.show()
     nparray = np.zeros((240,320,frames))
+    z = np.zeros((240,320))
     i=j=count = 0
     k = -1
     for j in range (0,cols):
-        count = count + 1
+        count = count + 1  #Go upto 240 columns, 
         if (j%240 == 0):
-            count = 0
-            k = k + 1
-            print "Reading Frame %s"%k
+            count = 0      #and reset again
+            k = k + 1      #Increment dimension
+            print "Columns Done: %s"%j
+            print "Frames Done:  %s"%k
+            pl.imshow(z, cmap=pl.cm.rainbow, 
+                       vmin = min_temp, vmax=max_temp)
+            pl.colorbar()
+            pl.draw()
+            time.sleep(0.005)
+            pl.clf()
         for i in range (0,320):
             nparray[count][i][k] = csvfile[j][i]
-            
+            z[count][i] = nparray[count][i][k]
     return nparray
 
-def np2img(path):
+
+def np2img():
     i=j=0
-    nparray = csv2np(path)
+    nparray = csv2np()
     print "Processing Frame 0"
     z = np.zeros((240,320))
-    for i in range (0,240):
-        for j in range(0,320):
-            z[i][j]=nparray[i][j][0]
+    pl.ion()
+    pl.show()
+    for k in range (0,frames):
+        pl.imshow(z, cmap=pl.cm.Accent, vmin=z.min(), vmax=z.max())
+        pl.colorbar()
+        pl.draw()
+        time.sleep(0.05)
+        pl.clf()
+        for i in range (0,240):
+            for j in range(0,320):
+                z[j][i]=nparray[j][i][k]
+                
             
     return z
 
-def img2plt(path):
+def img2plt():
     z = np.zeros((240,320))
-    z = np2img(path)
+    z = np2img()
     plt.imshow(z, cmap=plt.cm.gray, vmin=z.min(), vmax=z.max())
     colorbar()
     #savefig('foo1.png')
     plt.show()
+
 
 '''
 def animate(path):
@@ -56,4 +75,9 @@ def animate(path):
 
 if __name__ == '__main__':
     print "Starting"
-    img2plt('/home/mohit/devel/python/mohit/python/data.csv')
+    csvfile = genfromtxt('/home/mohit/devel/python/mohit/python/IR/data.csv',delimiter=',')
+    max_temp = csvfile.max()
+    min_temp = csvfile.min()
+    cols = csvfile.shape[0]
+    frames = cols/240
+    csv2np()
